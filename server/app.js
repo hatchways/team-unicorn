8e0cd5d241704938aec56c765fbb7743d8c07de8
swathi <swathi2689@gmail.com>
2020-09-14 19:20:21 -0600

    Initial Commit - Connecting MongoDB At;as

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { join } = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const connectDB = require("./config/db");
 
 const indexRouter = require("./routes/index");
 const pingRouter = require("./routes/ping");
@@ -10,6 +11,9 @@ const pingRouter = require("./routes/ping");
 const { json, urlencoded } = express;
 
 var app = express();
+
+// Connect Database
+connectDB();
 
 app.use(logger("dev"));
 app.use(json());
@@ -21,12 +25,12 @@ app.use("/", indexRouter);
 app.use("/ping", pingRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
