7b764a2067a8aaf477925816ef4a796001934782
Ugur Akin <ugurakinsoftware@gmail.com>
2020-09-19 15:20:17 -0700

    Auth middleware changed to look for token in cookies instead of header. Request body's student property renamed to user.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,19 +2,19 @@ const jwt = require("jsonwebtoken");
 const keys = require("../config/keys");
 
 module.exports = function (req, res, next) {
-  //get the token from the header
-  const token = req.header("x-auth-token");
+  // Get token from http-only cookie
+  const token = req.cookies["auth-token"];
 
   // Check if not token exists : 401  not authorized
   if (!token)
-    return res.status(401).json({ msg: "No token authoriazation denied" });
+    return res.status(401).json({ errorMessage: "No authorization token." });
 
   // Verify token
   try {
     const decoded = jwt.verify(token, keys.jwtSecret);
-    req.student = decoded.student;
+    req.user = decoded.user;
     next();
   } catch (err) {
-    res.status(401).json({ msg: "Token is not valid" });
+    res.status(401).json({ errorMessage: "Token is invalid or expired." });
   }
 };
