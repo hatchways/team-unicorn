0c2b07f9c168458f687709c3a92fd06bf206b0c9
Benny Wu <benny.l.wu@gmail.com>
2020-09-22 07:22:26 -0700

    combine dnd + memo + mui components. update grid

--- client/src/pages/draft-board-ui.js
+++ client/src/pages/draft-board-ui.js
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
-import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+import React, { useState, memo } from 'react';
+import {DragDropContext, Droppable} from 'react-beautiful-dnd';
 import { makeStyles } from '@material-ui/core/styles';
-import {Card, Button, Grid, Typography} from '@material-ui/core';
-import uuid from 'uuid/v4';
-import { theme } from '../themes/theme';
+import {Grid} from '@material-ui/core';
+import mockData from './mock-data';
+import Column from '../components/column'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    flexGrow: 1,
-    justifyContent: 'center',
-    height: '100%'
   },
   board: {
     display: "flex",
     backgroundColor: "red",
+    flexDirection: "column",
+    alignItems: "center"
   },
   column: {
     padding: theme.spacing(1),
@@ -52,207 +51,120 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const mockTasks = {
-  issues: [{ id: uuid(), desc: "Refactor css", deadline: '09/17/2020'}, { id: uuid(), desc: "Grid layout no longer responsive. Fix and optimize for viewports", deadline: '09/19/2020' },
-],
-  frontend: [{ id: uuid(), desc: "Drag columns", deadline: '09/17/2020' }, { id: uuid(), desc: "Style cards", deadline: '09/17/2020' },{ id: uuid(), desc: "Add context and state management", deadline: '09/19/2020' }
-],
-  progress: [{ id: uuid(), desc: "Create mock kanban board layout", deadline: '09/16/2020' }, { id: uuid(), desc: "Style board to match mock in google drive", deadline: '09/17/2020' }],
-  review: [{ id: uuid(), desc: "Drag cards", deadline: '09/17/2020' },
-],
-  completed: [{ id: uuid(), desc: "Research and choose react drag-and-drop library", deadline: '09/15/2020' },
-  { id: uuid(), desc: "Mock up data", deadline: '09/17/2020' },]
-    
+// performance optimization. prevents re-render when components are dragged all over w/memo
+const InnerList = memo((props) => {
+  const {column, taskMap, index} = props;
+  const tasks = column.taskIds.map(taskId => taskMap[taskId])
+  console.log('render')
+  return <Column column={column} tasks={tasks} index={index} />
 }
-
-const mockColumns = {
-  [uuid()]: {
-    name: "New Issues",
-    items: mockTasks.issues
-  },
-  [uuid()]: {
-    name: "Frontend",
-    items: mockTasks.frontend
-  },
-  [uuid()]: {
-    name: "In Progress",
-    items: mockTasks.progress
-  },
-  [uuid()]: {
-    name: "In Review",
-    items: mockTasks.review
-  },
-  [uuid()]: {
-    name: "Completed",
-    items: mockTasks.completed
-  }
-};
-
-const onDragEnd = (result, columns, setColumns) => {
-  if (!result.destination) return;
-  const { source, destination, type } = result;
-
-  if (type === 'column') {
-    const newColumnOrder = Array.from(Object.values(columns))
-    console.log(newColumnOrder)
-    const [removed] = newColumnOrder.splice(source.index, 1)
-    newColumnOrder.splice(destination.index, 0, removed)
-    setColumns(newColumnOrder)
-  } else {
-    if (source.droppableId !== destination.droppableId) {
-      const sourceColumn = columns[source.droppableId];
-      const destColumn = columns[destination.droppableId];
-      const sourceItems = [...sourceColumn.items];
-      const destItems = [...destColumn.items];
-      const [removed] = sourceItems.splice(source.index, 1);
-      destItems.splice(destination.index, 0, removed);
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...sourceColumn,
-          items: sourceItems
-        },
-        [destination.droppableId]: {
-          ...destColumn,
-          items: destItems
-        }
-      });
-    } else {
-      const column = columns[source.droppableId];
-      const copiedItems = [...column.items];
-      const [removed] = copiedItems.splice(source.index, 1);
-      copiedItems.splice(destination.index, 0, removed);
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...column,
-          items: copiedItems
-        }
-      });
-    }
-  }
-};
+);
 
 export default function KanbanBoard() {
-  const [columns, setColumns] = useState(mockColumns);
+  const [data, setData] = useState(mockData);
   const classes = useStyles();
+  const onDragEnd = result => {
+    const { destination, source, draggableId, type } = result;
 
-  console.log({columns})
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    if (type === 'column') {
+      const newColumnOrder = Array.from(data.columnOrder);
+      newColumnOrder.splice(source.index, 1);
+      newColumnOrder.splice(destination.index, 0, draggableId);
+
+      const newState = {
+        ...data,
+        columnOrder: newColumnOrder
+      }
+
+      setData(newState)
+      return;
+    }
+
+    const start = data.columns[source.droppableId];
+    const finish = data.columns[destination.droppableId];
+
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+  
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds,
+      };
+  
+      const newState = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newColumn.id]: newColumn,
+        },
+      }
+      setData(newState)
+      return;
+    }
+
+      // between lists
+      const startTaskIds = Array.from(start.taskIds);
+      startTaskIds.splice(source.index, 1);
+      const newStart = {
+        ...start,
+        taskIds: startTaskIds,
+      };
+  
+      const finishTaskIds = Array.from(finish.taskIds);
+      finishTaskIds.splice(destination.index, 0, draggableId);
+      const newFinish = {
+        ...finish,
+        taskIds: finishTaskIds,
+      };
+  
+      const newState = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newStart.id]: newStart,
+          [newFinish.id]: newFinish,
+        },
+      };
+      setData(newState);
+  };
 
   return (
-    <div className={classes.root}>
-      <DragDropContext
-        onDragEnd={result => onDragEnd(result, columns, setColumns)}
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable 
+        droppableId="all-columns"
+        direction="horizontal"
+        type="column"
       >
-      <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided, snapshot) => {
-          return (
-            Object.entries(columns).map(([id, column], index) => {
-            return (
-            <Grid
-              container
-              className={classes.board}
-              key={id}
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              spacing={1}
-            >
-            <Draggable
-              key={id}
-              draggableId={id}
-              index={index} 
-            >
-              {(provided) => {
-                return (
-
-              <Grid 
-                className={classes.column} 
-                {...provided.draggableProps}
-                ref={provided.innerRef}
-              >
-              <Typography 
-                {...provided.dragHandleProps}
-                variant="h4" 
-                align="center" 
-                gutterBottom
-              >{column.name}</Typography>
-                <Droppable droppableId={id} key={id} type="task">
-                  {(provided, snapshot) => {
-                    return (
-                      <Grid
-                        className={classes.drag}
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        style={{
-                          background: snapshot.isDraggingOver
-                            ? "#E6ECFC"
-                            : "#F4F6FF",
-                        }}
-                      >
-                        {column.items.map((item, index) => {
-                          return (
-                            <Draggable
-                              key={item.id}
-                              draggableId={item.id}
-                              index={index}
-                            >
-                              {(provided, snapshot) => {
-                                return (
-                                  <Card
-                                    ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    style={{
-                                      backgroundColor: snapshot.isDragging
-                                        ? theme.palette.text.secondary
-                                        : "#FFFFFF",
-                                      ...provided.draggableProps.style
-                                    }}
-                                    className={classes.card}
-                                  >
-                                  <Typography gutterBottom>
-                                    {item.desc}
-                                  </Typography>
-                                  <div style={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                  }}>
-                                    <Typography style={{textAlign: 'right'}}variant="caption">
-                                      id: {item.id.substring(0,5)}
-                                    </Typography>
-                                    <Typography style={{textAlign: 'right'}}variant="caption">
-                                      deadline: {item.deadline}
-                                    </Typography>
-                                  </div>
-                                  </Card>
-                                );
-                              }}
-                            </Draggable>
-                          );
-                        })}
-                        {provided.placeholder}
-                        <Button className={classes.button} size="large">
-                          <Typography>Add a card</Typography>
-                        </Button>
-                      </Grid>
-                    );
-                  }}
-                </Droppable>
-              </Grid>
-                )
-              }}
-
-            </Draggable>
+        {(provided) =>
+          <Grid 
+            container 
+            className={classes.root}
+            {...provided.droppableProps}
+            innerRef={provided.innerRef}
+          >
+            {data.columnOrder.map((columnId, index) => {
+                const column = data.columns[columnId];
+                return <InnerList key={column.id} column={column} taskMap={data.tasks} index={index} />;
+              })
+            }
             {provided.placeholder}
-            </Grid>
-          );
-        })
-          )
-        }}
-      </Droppable> 
-        
-      </DragDropContext>
-    </div>
+          </Grid>
+          
+        }
+      </Droppable>
+    </DragDropContext>
   );
-
 }
