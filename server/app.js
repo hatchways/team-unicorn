fdbefa62d57135386dc2eaf142e11a9a06b51b47
Benny Wu <benny.l.wu@gmail.com>
2020-10-01 08:38:06 -0700

    setup stripe one time payment and subscribe

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const createError = require("http-errors");
 const express = require("express");
 const { join } = require("path");
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const connectDB = require("./config/db");
@@ -9,7 +11,6 @@ const indexRouter = require("./routes/index");
 const pingRouter = require("./routes/ping");
 const uploadRouter = require("./routes/api/upload");
 const userRouter = require("./routes/user");
-
 const cardsRouter = require("./routes/api/cards");
 const colRouter = require("./routes/api/columns");
 const boardRouter = require("./routes/api/boards");
@@ -29,12 +30,12 @@ app.use(express.static(join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/ping", pingRouter);
-app.use("/api/upload", uploadRouter)
+app.use("/api/upload", uploadRouter);
 app.use("/api/cards", cardsRouter);
 app.use("/api/columns", colRouter);
 app.use("/api/boards", boardRouter);
-
 app.use("/user", userRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -50,5 +51,47 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({ error: err });
 });
+
+// STRIPE 
+app.use(cors());
+app.use(bodyParser.json());
+
+const stripe = require('stripe')(process.env.STRIPESECRET);
+
+app.post('/pay', async (req, res) => {
+  const {email} = req.body;
+  
+  const paymentIntent = await stripe.paymentIntents.create({
+      amount: 500,
+      currency: 'usd',
+      metadata: {integration_check: 'accept_a_payment'},
+      receipt_email: email,
+    });
+
+    res.json({'client_secret': paymentIntent['client_secret']})
+})
+
+app.post('/subscribe', async (req, res) => {
+  const {email, payment_method} = req.body;
+
+  const customer = await stripe.customers.create({
+    payment_method: payment_method,
+    email: email,
+    invoice_settings: {
+      default_payment_method: payment_method,
+    },
+  });
+
+  const subscription = await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [{ plan: 'price_1HX6TzD9AsNutHPovtPPJ9yY' }],
+    expand: ['latest_invoice.payment_intent']
+  });
+  
+  const status = subscription['latest_invoice']['payment_intent']['status'] 
+  const client_secret = subscription['latest_invoice']['payment_intent']['client_secret']
+
+  res.json({'client_secret': client_secret, 'status': status});
+})
 
 module.exports = app;
